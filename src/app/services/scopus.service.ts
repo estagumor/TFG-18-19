05148843b0498e3bbbd2df36793db806bba1b041
Manuel Herrera <dobloq@gmail.com>
2019-02-23 21:40:56 +0100

    Modified scopus request

--- src/app/services/scopus.service.ts
+++ src/app/services/scopus.service.ts
@@ -11,9 +11,10 @@ export class ScopusService {
 
   getAuthor(){
     const httpOptions = {headers: new HttpHeaders({
+      'Accept': 'application/json',
       'Content-Type': 'application/json',
       'X-ELS-APIKEY': 'f7f75a8f1e48b03f87da28cc8eb055b7'
     })}
-    return this._http.get("https://api.elsevier.com/content/author?author_id=15021461000",httpOptions)
+    return this._http.get("https://api.elsevier.com/content/search/author/query=AU-ID(15021461000)",httpOptions)
   }
 }
