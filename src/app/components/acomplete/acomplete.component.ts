6ccdf71223d1f33d81161f1f0c7c9534d170069c
Wordsan <esagmo96@gmail.com>
2019-02-28 13:43:54 +0100

    Autocomplete component integrated

---
+++ src/app/components/acomplete/acomplete.component.ts
@@ -0,0 +1,83 @@
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { ElementRef, ViewChild, Component, OnInit,Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatAutocompleteSelectedEvent, MatChipInputEvent, MatAutocomplete } from '@angular/material';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-acomplete',
+  templateUrl: './acomplete.component.html',
+  styleUrls: ['./acomplete.component.css']
+})
+export class AcompleteComponent implements OnInit {
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  basicCtrl = new FormControl();
+  filteredList: Observable<string[]>;
+  @Input() finalList: string[] = [];
+  @Input() allList: string[] = [];
+
+  @ViewChild('basicInput') basicInput: ElementRef<HTMLInputElement>;
+  @ViewChild('auto') matAutocomplete: MatAutocomplete;
+
+  constructor() {
+    this.filteredList = this.basicCtrl.valueChanges.pipe(
+      startWith(null),
+      map((element: string | null) => element ? this._filter(element) : this.allList.slice()));
+  }
+
+  add(event: MatChipInputEvent): void {
+    // Add element only when MatAutocomplete is not open
+    // To make sure this does not conflict with OptionSelected Event
+    if (!this.matAutocomplete.isOpen) {
+      const input = event.input;
+      const value = event.value;
+
+      // Add our element
+      if ((value || '').trim()) {
+        this.finalList.push(value.trim());
+      }
+
+      // Reset the input value
+      if (input) {
+        input.value = '';
+      }
+
+      this.basicCtrl.setValue(null);
+    }
+  }
+
+  remove(element: string): void {
+    const index = this.finalList.indexOf(element);
+
+    if (index >= 0) {
+      this.finalList.splice(index, 1);
+    }
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.finalList.push(event.option.viewValue);
+    this.basicInput.nativeElement.value = '';
+    this.basicCtrl.setValue(null);
+  }
+
+  private _filter(value: string): string[] {
+    if(value){
+      const filterValue = value.toLowerCase();
+      return this.allList.filter(option => option.toLowerCase().includes(filterValue)).filter(option => !this.finalList.includes(option));
+    }
+  }
+
+  ngOnInit() {
+    this.filteredList = this.basicCtrl.valueChanges // Para el autocompletado
+    .pipe(
+    startWith(''),
+    map(value => this._filter(value))
+    ); 
+  }
+}
+
--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -4,13 +4,6 @@ import { Project } from '../../models/project';
 import { ProjectService } from '../../services/project.service';
 import 'rxjs/add/operator/map';
 
-import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
-
-import {COMMA, ENTER} from '@angular/cdk/keycodes';
-import {MatAutocompleteSelectedEvent, MatChipInputEvent, MatAutocomplete} from '@angular/material';
-
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
@@ -18,41 +11,25 @@ import {MatAutocompleteSelectedEvent, MatChipInputEvent, MatAutocomplete} from '
   providers: [ProjectService]
 })
 export class ProjectsComponent implements OnInit {
-  public project: Project;
-  public responseFind: Project;
-  public responseCreate: Project;
-  public projectId: any;
-  public projectId2: any;
-  public listado: Array<Project>; // La variable donde se guarda la lista y despues sale por consola
-  public researchControl = new FormControl(); // El formulario
-  public workerControl = new FormControl();
-  public hiredControl = new FormControl();
-  public leaderControl = new FormControl();
+
+  //AUTOCOMPLETE
   public researchers: string[] = ['Javier Troya', 'Carlos Muller', 'Jose A. Parejo', 'Manuel Resinas']; // La lista que sale en el input al escribir
   public hireds: string[] = [];
   public finalResearchers: string[] = [];
   public finalWorkers: string[] = [];
   public finalHireds: string[] = [];
   public finalLeaders: string[] = [];
-  public researchVisible = true;
-  public selectable = true;
-  public removable = true;
-  public addOnBlur = true;
-  public separatorKeysCodes: number[] = [ENTER,COMMA];
-  public researchersfilteredOptions: Observable<string[]>;
-  public workersfilteredOptions: Observable<string[]>;
-  public hiredsfilteredOptions: Observable<string[]>;
-  public leadersfilteredOptions: Observable<string[]>;
+
+  public project: Project;
+  public responseFind: Project;
+  public responseCreate: Project;
+  public projectId: any;
+  public projectId2: any;
+  public listado: Array<Project>; // La variable donde se guarda la lista y despues sale por consola
+  
+  //IMPROVES
   public duration: string = "1";
 
-  @ViewChild('researcherInput') researcherInput: ElementRef<HTMLInputElement>;
-  @ViewChild('workerInput') workerInput: ElementRef<HTMLInputElement>;
-  @ViewChild('hiredInput') hiredInput: ElementRef<HTMLInputElement>;
-  @ViewChild('leaderInput') leaderInput: ElementRef<HTMLInputElement>;
-  @ViewChild('autoautoResearchers') MatAutocompleteR: MatAutocomplete;
-  @ViewChild('autoWorkers') MatAutocompleteW: MatAutocomplete;
-  @ViewChild('autoHireds') MatAutocompleteH: MatAutocomplete;
-  @ViewChild('autoLeaders') MatAutocompleteL: MatAutocomplete;
   constructor(
     private _route: ActivatedRoute, // Para señalar el link del menu que esta activo
     private _router: Router, // Para hacer el menu de navegacion
@@ -60,181 +37,9 @@ export class ProjectsComponent implements OnInit {
   ) {
     this.project = new Project([], [], [], '', '', [], '', '', '', '', null, null, null, [], []);
   }
-
-  researchAdd(event: MatChipInputEvent): void {
-    if(!this.MatAutocompleteR.isOpen) {
-      const input = event.input;
-      const value = event.value;
-
-      if ((value || '').trim()) {
-        this.finalResearchers.push(value.trim());
-      }
-
-      if (input) {
-        input.value = '';
-      }
-
-      this.researchControl.setValue(null);
-    }
-  }
-
-  workerAdd(event: MatChipInputEvent): void {
-    if(!this.MatAutocompleteW.isOpen) {
-      const input = event.input;
-      const value = event.value;
-
-      if ((value || '').trim()) {
-        this.finalWorkers.push(value.trim());
-      }
-
-      if (input) {
-        input.value = '';
-      }
-
-      this.workerControl.setValue(null);
-    }
-  }
-
-  hiredAdd(event: MatChipInputEvent): void {
-    if(!this.MatAutocompleteH.isOpen) {
-      const input = event.input;
-      const value = event.value;
-
-      if ((value || '').trim()) {
-        this.finalHireds.push(value.trim());
-      }
-
-      if (input) {
-        input.value = '';
-      }
-
-      this.hiredControl.setValue(null);
-    }
-  }
-
-  leaderAdd(event: MatChipInputEvent): void {
-    if(!this.MatAutocompleteL.isOpen) {
-      const input = event.input;
-      const value = event.value;
-
-      if ((value || '').trim()) {
-        this.finalLeaders.push(value.trim());
-      }
-
-      if (input) {
-        input.value = '';
-      }
-
-      this.leaderControl.setValue(null);
-    }
-  }
-
-  researchRemove(researcher: string): void {
-    const index = this.finalResearchers.indexOf(researcher);
-
-    if (index >=0) {
-      this.finalResearchers.splice(index, 1);
-    }
-  }
-
-  workerRemove(worker: string): void {
-    const index = this.finalWorkers.indexOf(worker);
-
-    if (index >=0) {
-      this.finalWorkers.splice(index, 1);
-    }
-  }
-
-  hiredRemove(hired: string): void {
-    const index = this.finalHireds.indexOf(hired);
-
-    if (index >=0) {
-      this.finalHireds.splice(index, 1);
-    }
-  }
-
-  leaderRemove(leader: string): void {
-    const index = this.finalLeaders.indexOf(leader);
-
-    if (index >=0) {
-      this.finalLeaders.splice(index, 1);
-    }
-  }
-
-  researchSelected(event: MatAutocompleteSelectedEvent): void {
-    this.finalResearchers.push(event.option.viewValue);
-    this.researcherInput.nativeElement.value = '';
-    this.researchControl.setValue(null);
-  }
-
-  workerSelected(event: MatAutocompleteSelectedEvent): void {
-    this.finalWorkers.push(event.option.viewValue);
-    this.workerInput.nativeElement.value = '';
-    this.workerControl.setValue(null);
-  }
-
-  hiredSelected(event: MatAutocompleteSelectedEvent): void {
-    this.finalHireds.push(event.option.viewValue);
-    this.hiredInput.nativeElement.value = '';
-    this.hiredControl.setValue(null);
-  }
-
-  leaderSelected(event: MatAutocompleteSelectedEvent): void {
-    this.finalLeaders.push(event.option.viewValue);
-    this.leaderInput.nativeElement.value = '';
-    this.leaderControl.setValue(null);
-  }
-
+ 
   ngOnInit() {
-    this.researchersfilteredOptions = this.researchControl.valueChanges // Para el autocompletado
-      .pipe(
-      startWith(''),
-      map(value => this._filterResearchers(value))
-      ); 
-    this.workersfilteredOptions = this.workerControl.valueChanges // Para el autocompletado
-      .pipe(
-      startWith(''),
-      map(value => this._filterWorkers(value))
-      ); 
-    this.hiredsfilteredOptions = this.hiredControl.valueChanges // Para el autocompletado
-      .pipe(
-      startWith(''),
-      map(value => this._filterHireds(value))
-      ); 
-    this.leadersfilteredOptions = this.leaderControl.valueChanges // Para el autocompletado
-      .pipe(
-      startWith(''),
-      map(value => this._filterLeaders(value))
-      ); 
   }
-
-  private _filterResearchers(value: string): string[] { // Para el autocompletado
-      if(value){
-        const filterValue = value.toLowerCase();
-        return this.researchers.filter(option => option.toLowerCase().includes(filterValue)).filter(option => !this.finalResearchers.includes(option));
-      }
-   } 
-
-   private _filterWorkers(value: string): string[] { // Para el autocompletado
-    if(value){
-      const filterValue = value.toLowerCase();
-      return this.researchers.filter(option => option.toLowerCase().includes(filterValue)).filter(option => !this.finalWorkers.includes(option));
-    }
-  } 
-
-   private _filterHireds(value: string): string[] { // Para el autocompletado
-    if(value){
-      const filterValue = value.toLowerCase();
-      return this.hireds.filter(option => option.toLowerCase().includes(filterValue)).filter(option => !this.finalHireds.includes(option));
-    }
-  }
-
-  private _filterLeaders(value: string): string[] { // Para el autocompletado
-      if(value){
-        const filterValue = value.toLowerCase();
-        return this.researchers.filter(option => option.toLowerCase().includes(filterValue)).filter(option => !this.finalLeaders.includes(option));
-      }
- } 
 
   onSubmit() {
     this.responseCreate = new Project([], [], [], '', '', [], '', '', '', '', null, null, null, [], []); // Instancia para guardar el resultado
@@ -242,6 +47,10 @@ export class ProjectsComponent implements OnInit {
     this.project.workTeam = this.finalWorkers;
     this.project.hiredStaff = this.finalHireds;
     this.project.leader = this.finalLeaders;
+    console.log("research Team " + this.finalResearchers);
+    console.log("work Team " + this.finalWorkers);
+    console.log("hired Staff " + this.finalHireds);
+    console.log("leaders " + this.finalLeaders);
     /*
     console.log(this.project.startDate);
     this.project.endDate = new Date();
