5112bdc31ec6993d93fcd5a4584421535db8d1d3
estagumor <esagmo96@gmail.com>
2019-03-30 13:59:01 +0100

    Person models

--- models/person.js
+++ models/person.js
@@ -4,7 +4,26 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var PersonSchema = Schema({
-	
+    name: {type: String, required: true},
+    surname: {type: String, required: true},
+    email: {type: String, required: true},
+    photo: {type: String, required: true},
+    telf: {type: String, required: true},
+    allowed: {type: Boolean, required: true},
+    job: {
+        type: String,
+        enum: ['RESEARCHER','HIRED']
+    },
+
+    //If is researcher
+    office: {type: String},
+    scopusId: {type: String},
+    professionalStatus: {
+        type: String,
+        enum: ['NONE','RESEARCHER','STUDENT','TECHNICIAN']
+    },
+    urls: {type: [String]},
+    active: {type: Boolean}
 });
 
 module.exports = mongoose.model('Person', PersonSchema);
--- src/app/models/person.ts
+++ src/app/models/person.ts
@@ -1,9 +1,22 @@
 export class Person {
-    constructor(
-        
+    constructor( 
+        public name: String,
+        public surname: String,
+        public email: String,
+        public photo: String,
+        public telf: String,
+        public allowed: Boolean,
+        public job: String,
+        public office: String,
+        public scopusId: String,
+        public professionalStatus: String,
+        public urls: String,
+        public active: Boolean
     ) {}
 
     static getFields() {
-        return {}
+        return {name: "Nombre", surname: "Apellidos", email: "Correo", photo: "Foto",  
+        telf: "Telefono", allowed: "Con acceso", job: "Puesto", office: "Despacho", scopusId: "Id de Scopus", professionalStatus: "Estado profesional", 
+        urls: "Paginas personales", active: "Activa"}
     }
 }
