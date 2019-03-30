7b056855b29101069721512e3fdc632d561988ff
estagumor <esagmo96@gmail.com>
2019-03-30 20:57:46 +0100

    Controller of the person service done

---
+++ controllers/person.js
@@ -0,0 +1,118 @@
+'use strict' // Para usar cosas buenas de js
+var mongoose = require('mongoose');
+var Person = require('../models/person'); // Aporta el modelo y los metodos de la bd
+var db = mongoose.connection;
+// 200 -> OK, 201 -> Created, 400 -> Bad Request, 500 -> Internal Server Error, 503 -> Service Unavailable
+var controller = {
+
+	savePerson: function (req, res) { // Metodo para crear proyectos
+		var person = req.body; // Recoje los parametros que le llegan y los mete en un project nuevo
+
+		Person.create(person, (err) => { // Intenta guardarlo y segun vaya responde
+			if (err) {
+				let errores = {}
+				return res.status(500).send({ message: err });
+			}
+			return res.status(201).send({ "per": per });
+		});
+	},
+
+	getPerson: function (req, res) {
+		var personId = req.params.id;
+
+		if (personId == null) {
+			return res.status(400).send({ message: 'La persona no existe' })
+		}
+
+		Person.findById(personId, (err, person) => {
+			if (err) return res.status(500).send({
+				message: err
+			});
+
+			else if (!person) return res.status(404).send({
+				message: "There is no person with that id"
+			});
+
+			return res.status(200).send({
+				person
+			});
+		});
+    },
+
+	getPersons: function (req, res) {
+		let limit = req.query.limit ? parseInt(req.query.limit) : 25;
+		let offset = req.query.offset ? parseInt(req.query.offset) : 0;
+		let total
+		Person.estimatedDocumentCount({}, (err, number) => {
+			total = number;
+		})
+
+		Person.find({}, null, { limit: limit, skip: offset }, (err, persons) => {
+			if (err) return res.status(500).send({ message: err })
+			if (!persons) return res.status(404).send({ message: 'There are no persons to show' })
+			// if (!req.body) // Esto esta aqui porque en el test le paso un string para que ignore esta parte, no he conseguido hacer stub del metodo que genera este dato
+			// 	res.setHeader('X-WP-Total', total);
+			return res.status(200).send({ "persons": persons });
+		})
+	},
+
+    //PUEDE SERVIR MAS ADELANTE
+	// findByTitle: function (req, res) {
+	// 	var titulo = req.params.title;
+	// 	if (titulo == null) {
+	// 		return res.status(400).send({ message: 'Title is empty' })
+	// 	}
+
+	// 	Project.find({ title: titulo }).exec((err, projects) => {
+	// 		if (err) return res.status(500).send({ message: err })
+	// 		if (!projects) return res.status(503).send({ message: 'There are no projects to show' })
+	// 		return res.status(200).send({ projects });
+	// 	});
+
+	// },
+
+	// findByReference: function (req, res) {
+	// 	var referencia = req.params.reference;
+	// 	if (referencia == null) {
+	// 		return res.status(400).send({ message: 'Reference is empty' })
+	// 	}
+
+	// 	Project.find({ reference: referencia }).exec((err, projects) => {
+	// 		if (err) return res.status(500).send({ message: err })
+	// 		if (!projects) return res.status(503).send({ message: 'There are no projects to show' })
+	// 		return res.status(200).send({ projects });
+	// 	});
+	// },
+
+	updatePerson: function (req, res) {
+		var personId = req.params.id;
+		var datosUpdate = req.body;
+
+		Person.findOneAndUpdate(personId, datosUpdate, { new: true }, (err, personUpdated) => {
+			if (err) return res.status(500).send({ message: err });
+
+			if (!personUpdated) return res.status(503).send({ message: "Error when trying to update the person" });
+
+			return res.status(200).send({
+				person: personUpdated
+			})
+		});
+	},
+
+	deletePerson: function (req, res) {
+		var personId = req.params.id;
+		Person.findByIdAndDelete(personId, (err, personDeleted) => {
+			if (err) return res.status(500).send({ message: err });
+
+			if (!personDeleted) return res.status(503).send({ message: "Error when trying to delete the person" });
+
+			return res.status(200).send({
+				person: personDeleted
+			})
+		});
+	}
+
+
+};
+
+module.exports = controller;
