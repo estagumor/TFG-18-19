eac1bfb27f64811e6f84aaa584d97fb37e464bf7
estagumor <esagmo96@gmail.com>
2019-08-04 20:10:29 +0200

    Display test started

---
+++ src/app/tests/display.component.spec.ts
@@ -0,0 +1,54 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+
+import { DisplayComponent } from '../components/display/display.component';
+import { By } from '@angular/platform-browser';
+
+xdescribe('DisplayComponent', () => {
+  let component: DisplayComponent;
+  let fixture: ComponentFixture<DisplayComponent>;
+  
+  beforeEach(async(() => {
+
+    TestBed.configureTestingModule({
+      declarations: [ DisplayComponent ],
+      imports: [HttpClientModule],
+      providers: [
+        {provide: MatDialogRef, useValue: {}},
+        {provide: MAT_DIALOG_DATA, useValue: {}}
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DisplayComponent);
+    component = fixture.componentInstance;
+
+    // find the hero's DebugElement and element
+    let titleDe = fixture.debugElement.query(By.css('.modal-dialog.modal-title.text-muted'));
+    let restDe = fixture.debugElement.query(By.css('.modal-dialog.modal-body'));
+    
+    let titleEl = titleDe.nativeElement;
+    let restEl = restDe.nativeElement;
+    // mock the hero supplied by the parent component
+    let expectedObj = { title: 'Prueba', array: ['Pepe','Julia','Ana'] };
+    // simulate the parent setting the input property with that hero
+    component.objeto = expectedObj;
+
+
+    // trigger initial data binding
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  xit('should display our object correctly', () => {
+    // const expectedPipedName = expectedHero.name.toUpperCase();
+    // expect(heroEl.textContent).toContain(expectedPipedName);
+  });
+
+});
