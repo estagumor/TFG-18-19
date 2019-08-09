6d2a3ad8d16aad0dc24e5feecc8fd0ab4a611a15
estagumor <esagmo96@gmail.com>
2019-08-09 21:55:17 +0200

    More unuseful testing

--- src/app/tests/persons/person-create.component.spec.ts
+++ src/app/tests/persons/person-create.component.spec.ts
@@ -1,18 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { NgForm } from '@angular/forms';
-import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'; 
 import { PersonCreateComponent } from '../../components/person-create/person-create.component';
 import { PersonService } from '../../services/person.service';
-import { RouterModule, Router, ActivatedRoute, Params } from '@angular/router';
+import { RouterModule, ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs';
+import { DebugElement } from '@angular/core';
 
 class MockActivatedRoute extends ActivatedRoute {
   constructor() {
-      super();
-      this.params = of({id: "5"});
+    super();
+    this.params = of({ id: "5" });
   }
 
-  setParamsMock (params) {
+  setParamsMock(params) {
     this.params = params
   }
 }
@@ -25,46 +26,87 @@ xdescribe('PersonCreateComponent', () => {
   let activatedRouteStub: MockActivatedRoute;
   let element: HTMLElement;
   let createSpy;
+  let updateSpy;
+  let getSpy;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PersonCreateComponent ],
-      imports: [ NgForm, NgModule ],
-      providers: [{provide: PersonService, useValue: personServiceStub }]
+      declarations: [PersonCreateComponent],
+      imports: [ReactiveFormsModule, FormsModule, HttpClientModule, RouterModule.forRoot([])],
+      providers: [{ provide: PersonService, useValue: personServiceStub }]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PersonCreateComponent);
     component = fixture.componentInstance;
-    // personServiceStub = {
-
-    // }
+    const bannerDe: DebugElement = fixture.debugElement;
+    var bannerEl: HTMLElement = bannerDe.nativeElement;
+    element = bannerEl;
+    activatedRouteStub = new MockActivatedRoute();
     personService = fixture.debugElement.injector.get(PersonService);
     createSpy = spyOn(personService, "create").and.returnValue(of(true));
+    updateSpy = spyOn(personService, "updatePerson").and.returnValue(of({
+      "body": {
+        "person": {
+          "name": "José Antonio",
+          "surname": "Parejo",
+          "email": "japarejo at us punto es",
+          "photo": "https://www.isa.us.es/2.0/assets/img/members/picture-11995321950.jpg",
+          "telf": "954556877",
+          "allowed": true,
+          "job": "RESEARCHER",
+          "office": "I0.71",
+          "scopusId": "24802465400",
+          "professionalStatus": "RESEARCHER",
+          "urls": [],
+          "active": true,
+          "_id": "5cefa10805b13e789525ce33",
+        }
+      }
+    }));
+    getSpy = spyOn(personService, "getPerson").and.returnValue(of({
+      "body": {
+        "person": {
+          "name": "José Antonio",
+          "surname": "Parejo",
+          "email": "japarejo at us punto es",
+          "photo": "https://www.isa.us.es/2.0/assets/img/members/picture-11995321950.jpg",
+          "telf": "954556877",
+          "allowed": true,
+          "job": "RESEARCHER",
+          "office": "I0.71",
+          "scopusId": "24802465400",
+          "professionalStatus": "RESEARCHER",
+          "urls": [],
+          "active": true,
+          "_id": "5cefa10805b13e789525ce33",
+        }
+      }
+    }));
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should get the person of the url', () => {
-    //TODO MIRAR IDS ACTUALES?
-    activatedRouteStub.setParamsMock([{"id?": ""}])
+  xit('should enter the edit mode and display the values of the person', () => {
+    activatedRouteStub.setParamsMock([{ "id?": "5cefa10805b13e789525ce33" }])
     fixture = TestBed.createComponent(PersonCreateComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    expect(getSpy).toHaveBeenCalled();
+    expect(element.querySelector("input[name='name']").textContent = "José Antonio");
   });
 
   it('bind person properties through a form', () => {
-    document.querySelector("input[name='name']").textContent = 'Manuel';
+    element.querySelector("input[name='name']").textContent = 'Manuel';
     expect(component.person.name = 'Manuel');
   });
 
-  it('should create a project', () => {
+  xit('should create a person', () => {
     let boton: HTMLButtonElement = element.querySelector("button[id='submitForm']")
-    //DEFINIR VARIABLES QUE SE REQUIERAN AQUI
     let job: HTMLInputElement = element.querySelector("select[name='job']")
     let professionalStatus: HTMLInputElement = element.querySelector("select[name='professionalStatus']")
 
@@ -77,5 +119,22 @@ xdescribe('PersonCreateComponent', () => {
     expect(createSpy).toHaveBeenCalled();
   });
 
-  //TODO falta por probar el update
+  xit('should edit a person', () => {
+    activatedRouteStub.setParamsMock([{ "id?": "sd56f4s" }])
+    component.edit = true
+    fixture.detectChanges();
+
+    let boton: HTMLButtonElement = element.querySelector("button[id='submitForm']")
+    let job: HTMLInputElement = element.querySelector("select[name='job']")
+    let professionalStatus: HTMLInputElement = element.querySelector("select[name='professionalStatus']")
+
+    job.value = 'RESEARCHER';
+    professionalStatus.value = 'NONE';
+    // Makes Angular detect changes in the page and simulate the click
+    fixture.detectChanges();
+    boton.click();
+    // Check that the create method has been called in the service
+    expect(updateSpy).toHaveBeenCalled();
+    expect(createSpy).toHaveBeenCalled();
+  });
 });
