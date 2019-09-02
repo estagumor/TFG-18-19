7464704006f1a9ec3e6e26515a7742214bfde03a
estagumor <esagmo96@gmail.com>
2019-09-02 21:13:06 +0200

    Bye bye consoles

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -11,12 +11,8 @@ var controller = {
 
 	populate: function (req, res) { 
 		Publication.deleteMany({}, () => {})
-		console.log("publication delete")
 		Person.deleteMany({}, () => {})
-		console.log("person delete")
 		Project.deleteMany({}, () => {})
-		console.log("project delete")
-
 		//Persons
 		var person1 = {
 			name: "José Antonio", 
@@ -240,10 +236,8 @@ var controller = {
 			return pubController.saveAll([pub1, pub2, pub3, pub4, pub5])
 		}
 		createProject.then(createPublication).then(createPerson).then(() => {
-			console.log("promises")
 			return res.status(201).send({});
 		}).catch(err => {
-			console.log(err)
 			return res.status(500).send({ message_es: "Error en el populado", message_en: "Error in the request", message_data: err });
 		})
 
